bbe1f134f55ff7d2463ca1d55b2ecf7832cce901
WW <ww6933@sina.com>
2021-03-17 17:54:31 +0800

    2021-03-17 1：整合redis 2：整合redisson

---
+++ mall-common/src/main/java/com/ww/mall/common/config/MyRedissonConfig.java
@@ -0,0 +1,24 @@
+package com.ww.mall.common.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Author:         ww
+ * Datetime:       2021\3\17 0017
+ * Description:    redisson 配置类
+ */
+@Configuration
+public class MyRedissonConfig {
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redisson() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        return Redisson.create(config);
+    }
+
+}
---
+++ mall-common/src/main/java/com/ww/mall/common/constant/RedisKey.java
@@ -0,0 +1,10 @@
+package com.ww.mall.common.constant;
+
+/**
+ * Author:         ww
+ * Datetime:       2021\3\17 0017
+ * Description:
+ */
+public class RedisKey {
+    public final static String PRODUCT_CATEGORY_DATA = "product:category:data";
+}
