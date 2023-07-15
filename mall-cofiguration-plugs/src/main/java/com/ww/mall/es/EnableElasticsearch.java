4b8f277d4413b00b52524c6e55d49fa66cede3fe
WW <ww6933@sina.com>
2023-07-15 17:39:13 +0800

    插件配置模块

---
+++ mall-cofiguration-plugs/src/main/java/com/ww/mall/es/EnableElasticsearch.java
@@ -0,0 +1,19 @@
+package com.ww.mall.es;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author ww
+ * @create 2023-07-15- 17:32
+ * @description:
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Import({MallElasticsearchAutoConfiguration.class})
+public @interface EnableElasticsearch {
+}
