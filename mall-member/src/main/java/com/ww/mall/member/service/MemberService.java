152d38b9d948366107ea5ee7a4f0933da9a92da3
WW <ww6933@sina.com>
2023-07-17 18:25:14 +0800

    补充

---
+++ mall-member/src/main/java/com/ww/mall/member/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.ww.mall.member.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ww.mall.member.entity.Member;
+
+/**
+ * @author ww
+ * @create 2023-07-17- 11:09
+ * @description:
+ */
+public interface MemberService extends IService<Member> {
+}
