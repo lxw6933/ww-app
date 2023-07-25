9c48db2aafdccf01e11ccfcc95980c33de130fcb
WW <ww6933@sina.com>
2023-07-25 16:05:55 +0800

    up

--- mall-coupon/src/test/java/com/ww/mall/coupon/CouponApplicationTests.java
+++ mall-coupon/src/test/java/com/ww/mall/coupon/CouponApplicationTests.java
@@ -10,26 +10,12 @@ import javax.annotation.Resource;
 @SpringBootTest
 class CouponApplicationTests {
 
-    @Resource
-    private UserMapper userMapper;
-
     @Test
     public void testSelect() {
-        System.out.println(("----- selectAll method test ------"));
-        QueryWrapper<User> wrapper = new QueryWrapper<>();
-        Page<User> page = new Page(2,2);
-//        List<User> userList = userMapper.selectList(null);
-        Page<User> userPageList = userMapper.selectPage(page, null);
-//        Assert.assertEquals(2, userList.size());
-//        userList.forEach(System.out::println);
-        userPageList.getRecords().forEach(System.out::println);
     }
 
     @Test
     public void save(){
-        User user = new User();
-        user.setName("大白2");
-        userMapper.insert(user);
     }
 
 
