e481d1f3016df414eb7c37ed649419f90b80ead0
WW <ww6933@sina.com>
2023-07-27 09:01:27 +0800

    补充

--- mall-configuration-plugs/src/main/java/com/ww/mall/rabbitmq/routekey/RouteKeyConstant.java
+++ mall-configuration-plugs/src/main/java/com/ww/mall/rabbitmq/routekey/RouteKeyConstant.java
@@ -10,11 +10,11 @@ public class RouteKeyConstant {
     /**
      * 用户注册key
      */
-    public final static String MALL_MEMBER_REGISTER_KEY = "mall.member.register.key";
+    public static final String MALL_MEMBER_REGISTER_KEY = "mall.member.register.key";
 
     /**
      * canal route key
      */
-    public final static String MALL_CANAL_KEY = "mall.canal.key";
+    public static final String MALL_CANAL_KEY = "mall.canal.key";
 
 }
