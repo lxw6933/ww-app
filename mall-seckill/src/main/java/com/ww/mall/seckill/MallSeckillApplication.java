fbdc07460bf0f0633d7627f4f11b4b70dcd0368c
WW <ww6933@sina.com>
2024-02-06 15:16:29 +0800

    seckill module

--- mall-seckill/src/main/java/com/ww/mall/seckill/MallSeckillApplication.java
+++ mall-seckill/src/main/java/com/ww/mall/seckill/MallSeckillApplication.java
@@ -1,8 +1,16 @@
 package com.ww.mall.seckill;
 
+import com.ww.mall.mongodb.EnableMallMongodb;
+import com.ww.mall.rabbitmq.EnableMallRabbitmq;
+import com.ww.mall.redis.EnableMallRedis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableMallRedis
+@EnableMallMongodb
+@EnableMallRabbitmq
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MallSeckillApplication {
 
