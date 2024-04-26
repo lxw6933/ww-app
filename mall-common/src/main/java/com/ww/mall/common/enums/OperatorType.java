d7f4d9ba6ae432b425de1c9068c848a294dba39d
WW <ww6933@sina.com>
2024-04-26 13:45:44 +0800

    add

---
+++ mall-common/src/main/java/com/ww/mall/common/enums/OperatorType.java
@@ -0,0 +1,25 @@
+package com.ww.mall.common.enums;
+
+/**
+ * @author ww
+ * @create 2024-04-26- 10:39
+ * @description:
+ */
+public enum OperatorType {
+
+    /**
+     * 其它
+     */
+    OTHER,
+
+    /**
+     * 后台用户
+     */
+    MANAGE,
+
+    /**
+     * 手机端用户
+     */
+    MOBILE
+
+}
