5ff59d5a1911b5cedd00962a1d149790229b7186
ww <ww6933@sina.com>
2024-04-26 21:22:19 +0800

    minio

--- mall-seckill/src/test/java/com/ww/mall/seckill/MallSeckillApplicationTests.java
+++ mall-seckill/src/test/java/com/ww/mall/seckill/MallSeckillApplicationTests.java
@@ -1,7 +1,6 @@
 package com.ww.mall.seckill;
 
 import com.ww.mall.minio.MallMinioTemplate;
-import com.ww.mall.minio.s3.MallMinioS3Template;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,19 +12,11 @@ class MallSeckillApplicationTests {
     @Resource
     private MallMinioTemplate mallMinioTemplate;
 
-    @Resource
-    private MallMinioS3Template mallMinioS3Template;
-
     @Test
     void removeBucket() {
         System.out.println("创建bucket" + mallMinioTemplate.createBucket("mall-demo"));
         System.out.println("列表bucket" + mallMinioTemplate.listBucketAllFile("mall-demo", true));
         System.out.println("删除bucket" + mallMinioTemplate.removeBucket("mall-demo"));
-    }
-
-    @Test
-    void s3() {
-        System.out.println(mallMinioS3Template);
     }
 
 }
