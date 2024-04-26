fccb89ec68f46778694b82614c83bccb567db702
WW <ww6933@sina.com>
2024-04-26 13:34:42 +0800

    敏感数据脱敏

---
+++ mall-common/src/main/java/com/ww/mall/common/annotation/Sensitive.java
@@ -0,0 +1,25 @@
+package com.ww.mall.common.annotation;
+
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.ww.mall.common.enums.SensitiveDataType;
+import com.ww.mall.common.serializer.SensitiveJsonSerializer;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author ww
+ * @create 2024-04-26- 10:46
+ * @description:
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@JacksonAnnotationsInside
+@JsonSerialize(using = SensitiveJsonSerializer.class)
+public @interface Sensitive {
+
+    SensitiveDataType sensitiveDataType();
+}
---
+++ mall-common/src/main/java/com/ww/mall/common/serializer/SensitiveJsonSerializer.java
@@ -0,0 +1,43 @@
+package com.ww.mall.common.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.ContextualSerializer;
+import com.ww.mall.common.annotation.Sensitive;
+import com.ww.mall.common.enums.SensitiveDataType;
+
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * @author ww
+ * @create 2024-04-26- 10:50
+ * @description:
+ */
+public class SensitiveJsonSerializer extends JsonSerializer<String> implements ContextualSerializer {
+
+    private SensitiveDataType sensitiveDataType;
+
+    @Override
+    public JsonSerializer<?> createContextual(SerializerProvider prov, BeanProperty property) throws JsonMappingException {
+        // 获取bean属性上的@Sensitive注解
+        Sensitive annotation = property.getAnnotation(Sensitive.class);
+        if (Objects.nonNull(annotation) && Objects.equals(String.class, property.getType().getRawClass())) {
+            // 获取数据敏感类型
+            this.sensitiveDataType = annotation.sensitiveDataType();
+            // 返回敏感数据序列化器
+            return this;
+        }
+        return prov.findValueSerializer(property.getType(), property);
+    }
+
+    @Override
+    public void serialize(String value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        // 序列化敏感数据字段
+        gen.writeString(sensitiveDataType.desensitizer.apply(value));
+    }
+
+}
