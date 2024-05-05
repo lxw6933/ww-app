3b77b899e1c63b6acec8d6e205543e9a211e3f03
ww <ww6933@sina.com>
2024-05-05 23:12:15 +0800

    add

--- mall-seckill/src/test/java/com/ww/mall/seckill/MallSeckillApplicationTests.java
+++ mall-seckill/src/test/java/com/ww/mall/seckill/MallSeckillApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
-import java.io.File;
 import java.io.FileInputStream;
 
 @SpringBootTest
