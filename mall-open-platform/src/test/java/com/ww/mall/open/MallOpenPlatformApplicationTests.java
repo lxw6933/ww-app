6670c158b71b4efb182a4b528efaa32fdf58ac41
ww <ww6933@sina.com>
2024-05-26 22:23:52 +0800

    add

---
+++ mall-open-platform/src/test/java/com/ww/mall/open/MallOpenPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ww.mall.open;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOpenPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
