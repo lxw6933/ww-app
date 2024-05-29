663346e2fb383262183d725d64f0072eb3d29904
WW <ww6933@sina.com>
2024-05-29 16:14:02 +0800

    add

--- mall-configuration-plugs/src/main/java/com/ww/mall/rabbitmq/enums/MqMsgStatus.java
+++ mall-configuration-plugs/src/main/java/com/ww/mall/rabbitmq/enums/MqMsgStatus.java
@@ -1,5 +1,7 @@
 package com.ww.mall.rabbitmq.enums;
 
+import lombok.Getter;
+
 import java.util.StringJoiner;
 
 /**
@@ -7,6 +9,7 @@ import java.util.StringJoiner;
  * @author: ww
  * @create: 2023/7/21 23:39
  **/
+@Getter
 public enum MqMsgStatus {
 
     DELIVER_SUCCESS("发送成功"),
@@ -14,17 +17,9 @@ public enum MqMsgStatus {
     CONSUMED_SUCCESS("消费成功"),
     CONSUMED_FAIL("消费失败");
 
-    private String text;
+    private final String text;
 
     MqMsgStatus(String text) {
-        this.text = text;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
         this.text = text;
     }
 
