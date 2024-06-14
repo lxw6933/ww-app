3f9481ecf462d9e54908f504bb56496b2340bf7a
WW <ww6933@sina.com>
2024-06-14 13:37:08 +0800

    add

---
+++ mall-configuration-plugs/src/main/java/com/ww/mall/redis/constant/LuaConstant.java
@@ -0,0 +1,65 @@
+package com.ww.mall.redis.constant;
+
+/**
+ * @author ww
+ * @create 2024-06-14- 13:34
+ * @description:
+ */
+public class LuaConstant {
+
+    public static final String DECREMENT_STOCK_LUA = "local current_stock = tonumber(redis.call('get', KEYS[1]) or 0);\n" +
+            "if current_stock >= tonumber(ARGV[1]) then\n" +
+            "    redis.call('decrby', KEYS[1], tonumber(ARGV[1]));\n" +
+            "    return current_stock - tonumber(ARGV[1]);\n" +
+            "else\n" +
+            "    return -1;\n" +
+            "end";
+    public static final byte[] DECREMENT_STOCK_LUA_BYTE = DECREMENT_STOCK_LUA.getBytes();
+
+    public static final String LOCK_STOCK_HASH_LUA = "local hashKey = KEYS[1]\n" +
+            "local decrementStock = tonumber(ARGV[1])\n" +
+            "local totalStock = tonumber(redis.call('hget', hashKey, 'totalStock'))\n" +
+            "local lockStock = tonumber(redis.call('hget', hashKey, 'lockStock'))\n" +
+            "local useStock = tonumber(redis.call('hget', hashKey, 'useStock'))\n" +
+            "if totalStock == nil or lockStock == nil or useStock == nil then\n" +
+            "   return -1\n" +
+            "end\n" +
+            "local availableStock = totalStock - lockStock - useStock\n" +
+            "if availableStock < decrementStock then\n" +
+            "   return -2\n" +
+            "end\n" +
+            "local newLockStock = lockStock + decrementStock\n" +
+            "redis.call('hset', hashKey, 'lockStock', newLockStock)\n" +
+            "return 1";
+    public static final byte[] LOCK_STOCK_HASH_LUA_BYTE = LOCK_STOCK_HASH_LUA.getBytes();
+
+    public static final String LOCK_STOCK_HASH_BATCH_LUA = "for i = 1, #KEYS do\n" +
+            "local hashKey = KEYS[i]\n" +
+            "local decrementStock = tonumber(ARGV[i])\n" +
+            "local totalStock = tonumber(redis.call('hget', hashKey, 'totalStock'))\n" +
+            "local lockStock = tonumber(redis.call('hget', hashKey, 'lockStock'))\n" +
+            "local useStock = tonumber(redis.call('hget', hashKey, 'useStock'))\n" +
+            "if totalStock == nil or lockStock == nil or useStock == nil then\n" +
+            "    return -1\n" +
+            "end\n" +
+            "local availableStock = totalStock - lockStock - useStock\n" +
+            "if availableStock < decrementStock then\n" +
+            "    return -2\n" +
+            "end\n" +
+            "local newLockStock = lockStock + decrementStock\n" +
+            "redis.call('hset', hashKey, 'lockStock', newLockStock)\n" +
+            "end\n" +
+            "return 1";
+    public static final byte[] LOCK_STOCK_HASH_BATCH_LUA_BYTE = LOCK_STOCK_HASH_BATCH_LUA.getBytes();
+
+    public static final String USE_STOCK_HASH_LUA =
+            "local hashKey = KEYS[1]\n" +
+                    "local lockStock = ARGV[1]\n" +
+                    "local useStock = ARGV[2]\n" +
+                    "local number = tonumber(ARGV[3])\n" +
+                    "redis.call('HINCRBYFLOAT', hashKey, lockStock, -number)\n" +
+                    "redis.call('HINCRBYFLOAT', hashKey, useStock, number)\n" +
+                    "return 1";
+    public static final byte[] USE_STOCK_HASH_LUA_BYTE = USE_STOCK_HASH_LUA.getBytes();
+    
+}
