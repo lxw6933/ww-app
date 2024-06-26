33ac96cd2358ee87c084e8b0f96449e5316aa439
ww <ww6933@sina.com>
2024-06-26 22:55:42 +0800

    add

--- mall-configuration-plugs/src/main/java/com/ww/mall/redis/constant/LuaConstant.java
+++ mall-configuration-plugs/src/main/java/com/ww/mall/redis/constant/LuaConstant.java
@@ -9,7 +9,7 @@ public class LuaConstant {
 
     public static final String DECREMENT_STOCK_LUA = "local current_stock = tonumber(redis.call('get', KEYS[1]) or 0);\n" +
             "if current_stock >= tonumber(ARGV[1]) then\n" +
-            "    redis.call('decrby', KEYS[1], tonumber(ARGV[1]));\n" +
+            "    redis.call('DECRBY', KEYS[1], tonumber(ARGV[1]));\n" +
             "    return current_stock - tonumber(ARGV[1]);\n" +
             "else\n" +
             "    return -1;\n" +
@@ -18,9 +18,9 @@ public class LuaConstant {
 
     public static final String LOCK_STOCK_HASH_LUA = "local hashKey = KEYS[1]\n" +
             "local decrementStock = tonumber(ARGV[1])\n" +
-            "local totalStock = tonumber(redis.call('hget', hashKey, 'totalStock'))\n" +
-            "local lockStock = tonumber(redis.call('hget', hashKey, 'lockStock'))\n" +
-            "local useStock = tonumber(redis.call('hget', hashKey, 'useStock'))\n" +
+            "local totalStock = tonumber(redis.call('HGET', hashKey, 'totalStock'))\n" +
+            "local lockStock = tonumber(redis.call('HGET', hashKey, 'lockStock'))\n" +
+            "local useStock = tonumber(redis.call('HGET', hashKey, 'useStock'))\n" +
             "if totalStock == nil or lockStock == nil or useStock == nil then\n" +
             "   return -1\n" +
             "end\n" +
@@ -29,16 +29,16 @@ public class LuaConstant {
             "   return -2\n" +
             "end\n" +
             "local newLockStock = lockStock + decrementStock\n" +
-            "redis.call('hset', hashKey, 'lockStock', newLockStock)\n" +
+            "redis.call('HSET', hashKey, 'lockStock', newLockStock)\n" +
             "return 1";
     public static final byte[] LOCK_STOCK_HASH_LUA_BYTE = LOCK_STOCK_HASH_LUA.getBytes();
 
     public static final String BATCH_LOCK_STOCK_HASH_LUA = "for i = 1, #KEYS do\n" +
             "local hashKey = KEYS[i]\n" +
             "local decrementStock = tonumber(ARGV[i])\n" +
-            "local totalStock = tonumber(redis.call('hget', hashKey, 'totalStock'))\n" +
-            "local lockStock = tonumber(redis.call('hget', hashKey, 'lockStock'))\n" +
-            "local useStock = tonumber(redis.call('hget', hashKey, 'useStock'))\n" +
+            "local totalStock = tonumber(redis.call('HGET', hashKey, 'totalStock'))\n" +
+            "local lockStock = tonumber(redis.call('HGET', hashKey, 'lockStock'))\n" +
+            "local useStock = tonumber(redis.call('HGET', hashKey, 'useStock'))\n" +
             "if totalStock == nil or lockStock == nil or useStock == nil then\n" +
             "    return -1\n" +
             "end\n" +
@@ -47,26 +47,33 @@ public class LuaConstant {
             "    return -2\n" +
             "end\n" +
             "local newLockStock = lockStock + decrementStock\n" +
-            "redis.call('hset', hashKey, 'lockStock', newLockStock)\n" +
+            "redis.call('HSET', hashKey, 'lockStock', newLockStock)\n" +
             "end\n" +
             "return 1";
     public static final byte[] BATCH_LOCK_STOCK_HASH_LUA_BYTE = BATCH_LOCK_STOCK_HASH_LUA.getBytes();
 
     public static final String USE_STOCK_HASH_LUA = "local hashKey = KEYS[1]\n" +
             "local number = tonumber(ARGV[1])\n" +
-            "local lockStock = tonumber(redis.call('hget', hashKey, 'lockStock'))\n" +
+            "local oldLockValue = redis.call('HGET', hashKey, 'lockStock')\n" +
+            "local oldUseValue = redis.call('HGET', hashKey, 'useStock')\n" +
+            "local lockStock = tonumber(oldLockValue)\n" +
             "if lockStock < number then\n" +
             "    return -1\n" +
             "end\n" +
-            "redis.call('HINCRBYFLOAT', hashKey, 'lockStock', -number)\n" +
-            "redis.call('HINCRBYFLOAT', hashKey, 'useStock', number)\n" +
+            "local lockResult = redis.call('HINCRBYFLOAT', hashKey, 'lockStock', -number)\n" +
+            "local useResult = redis.call('HINCRBYFLOAT', hashKey, 'useStock', number)\n" +
+            "if setResult1 == 0 and setResult2 == 0 then\n" +
+            "   redis.call('HSET', hashKey, 'lockStock', oldLockValue)\n" +
+            "   redis.call('HSET', hashKey, 'useStock', oldUseValue)\n" +
+            "   return -2\n" +
+            "end\n" +
             "return 1";
     public static final byte[] USE_STOCK_HASH_LUA_BYTE = USE_STOCK_HASH_LUA.getBytes();
 
     public static final String BATCH_USE_STOCK_HASH_LUA = "for i = 1, #KEYS do\n" +
             "local hashKey = KEYS[i]\n" +
             "local number = tonumber(ARGV[i])\n" +
-            "local lockStock = tonumber(redis.call('hget', hashKey, 'lockStock'))\n" +
+            "local lockStock = tonumber(redis.call('HGET', hashKey, 'lockStock'))\n" +
             "if lockStock < number then\n" +
             "    return -1\n" +
             "end\n" +
@@ -78,7 +85,7 @@ public class LuaConstant {
 
     public static final String ROLLBACK_STOCK_HASH_LUA = "local hashKey = KEYS[1]\n" +
             "local number = tonumber(ARGV[1])\n" +
-            "local lockStock = tonumber(redis.call('hget', hashKey, 'lockStock'))\n" +
+            "local lockStock = tonumber(redis.call('HGET', hashKey, 'lockStock'))\n" +
             "if lockStock < number then\n" +
             "    return -1\n" +
             "end\n" +
@@ -89,7 +96,7 @@ public class LuaConstant {
     public static final String BATCH_ROLLBACK_STOCK_HASH_LUA = "for i = 1, #KEYS do\n" +
             "local hashKey = KEYS[i]\n" +
             "local number = tonumber(ARGV[i])\n" +
-            "local lockStock = tonumber(redis.call('hget', hashKey, 'lockStock'))\n" +
+            "local lockStock = tonumber(redis.call('HGET', hashKey, 'lockStock'))\n" +
             "if lockStock < number then\n" +
             "    return -1\n" +
             "end\n" +
