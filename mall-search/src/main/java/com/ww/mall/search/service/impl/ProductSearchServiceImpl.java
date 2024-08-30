d86d15e378b69da3d22a725a09047b45349723b0
ww <ww6933@sina.com>
2024-08-30 14:07:55 +0800

    number str sort bug

--- mall-search/src/main/java/com/ww/mall/search/service/impl/ProductSearchServiceImpl.java
+++ mall-search/src/main/java/com/ww/mall/search/service/impl/ProductSearchServiceImpl.java
@@ -4,7 +4,6 @@ import com.ww.mall.search.entity.ProductSearch;
 import com.ww.mall.search.service.ProductSearchService;
 import com.ww.mall.search.view.bo.PortalProductSearchBO;
 import com.ww.mall.search.view.vo.PortalProductSearchVO;
-import com.ww.mall.web.cmmon.MallPage;
 import com.ww.mall.web.cmmon.MallPageResult;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
@@ -13,11 +12,13 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.query.Collation;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * @author ww
@@ -35,6 +36,8 @@ public class ProductSearchServiceImpl implements ProductSearchService {
     public MallPageResult<PortalProductSearchVO> portalProductSearch(PortalProductSearchBO portalProductSearchBO, String curAppKey) {
         String collectionName = "v2_product_search";
         boolean integralChannel = true;
+        // number str sort
+        Collation collation = Collation.of(Locale.CHINESE).numericOrdering(true);
         // query condition
         AggregationOperation matchAggregation = Aggregation.match(portalProductSearchBO.buildQueryCriteria());
         // sort condition
@@ -45,7 +48,8 @@ public class ProductSearchServiceImpl implements ProductSearchService {
         AggregationOperation skip = Aggregation.skip((long) (portalProductSearchBO.getPageNum() - 1) * portalProductSearchBO.getPageSize());
         AggregationOperation limit = Aggregation.limit(portalProductSearchBO.getPageSize());
         // build the aggregation pipeline
-        Aggregation aggregation = Aggregation.newAggregation(matchAggregation, sortAggregation, groupAggregation, skip, limit);
+        Aggregation aggregation = Aggregation.newAggregation(matchAggregation, sortAggregation, groupAggregation, skip, limit)
+                .withOptions(Aggregation.newAggregationOptions().collation(collation).build());
         // query aggregation data result
         AggregationResults<ProductSearch> productSearchAggregationResult = mongoTemplate.aggregate(aggregation, collectionName, ProductSearch.class);
         List<ProductSearch> productSearchResult = productSearchAggregationResult.getMappedResults();
