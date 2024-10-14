1f37983a25b0edab9b2fa583bfc66b2a8a68838b
WW <ww6933@sina.com>
2024-10-14 16:30:58 +0800

    adjust record queue

--- mall-configuration-plugs/src/main/java/com/ww/mall/redis/service/IssueCodeService.java
+++ mall-configuration-plugs/src/main/java/com/ww/mall/redis/service/IssueCodeService.java
@@ -1,10 +1,9 @@
-package com.ww.mall.redis.service.codes;
+package com.ww.mall.redis.service;
 
 import cn.hutool.core.date.DateUtil;
 import com.ww.mall.common.exception.ApiException;
 import com.ww.mall.mongodb.queue.RecordQueueComponent;
 import com.ww.mall.mongodb.repository.IssueCodeRecord;
-import com.ww.mall.redis.service.UniqueService;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RList;
 import org.redisson.api.RScript;
