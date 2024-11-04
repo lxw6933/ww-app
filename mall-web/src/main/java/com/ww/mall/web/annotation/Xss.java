59926f938882f2d79d68db4ade709cc6268223a8
WW <ww6933@sina.com>
2024-11-04 15:37:34 +0800

    add xss validator

---
+++ mall-web/src/main/java/com/ww/mall/web/annotation/Xss.java
@@ -0,0 +1,28 @@
+package com.ww.mall.web.annotation;
+
+import com.ww.mall.web.validator.XssValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author ww
+ * @create 2024-11-04- 15:34
+ * @description:
+ */
+@Target(value = { ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = XssValidator.class)
+public @interface Xss {
+
+    String message() default "非法输入, 检测到潜在的xss代码";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
+
