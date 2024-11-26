fb1d9e8bcd4224112337b63ef00e99d017bf08b7
WW <ww6933@sina.com>
2024-11-26 17:50:01 +0800

    【新增mongo主键常量】

--- mall-common/src/main/java/com/ww/mall/common/constant/Constant.java
+++ mall-common/src/main/java/com/ww/mall/common/constant/Constant.java
@@ -7,6 +7,8 @@ package com.ww.mall.common.constant;
  */
 public class Constant {
 
+    public static final String MONGO_PRIMARY_KEY = "_id";
+
     public static final String UTF_8 = "UTF-8";
 
     public static final String AES_ECB = "AES/ECB/PKCS5Padding";
