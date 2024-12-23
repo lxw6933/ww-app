0a5a355a301b8e92aab85dbd73fff9a7a109f164
WW <ww6933@sina.com>
2024-12-23 14:46:00 +0800

    remove code

--- mall-coupon/src/main/java/com/ww/mall/coupon/controller/DemoController.java
+++ mall-coupon/src/main/java/com/ww/mall/coupon/controller/DemoController.java
@@ -2,27 +2,17 @@ package com.ww.mall.coupon.controller;
 
 import com.ww.mall.common.exception.ApiException;
 import com.ww.mall.coupon.config.CouponProperties;
-import com.ww.mall.coupon.dao.CouponMapper;
-import com.ww.mall.coupon.entity.Coupon;
-import com.ww.mall.coupon.eunms.*;
 import com.ww.mall.coupon.service.CouponService;
 import com.ww.mall.coupon.view.bo.CouponPageBO;
 import com.ww.mall.rabbitmq.MallPublisher;
 import com.ww.mall.rabbitmq.exchange.ExchangeConstant;
 import com.ww.mall.rabbitmq.routekey.RouteKeyConstant;
-import com.ww.mall.web.config.SecretProperties;
-import com.ww.mall.web.config.thread.DefaultThreadPoolProperties;
 import lombok.extern.slf4j.Slf4j;
-import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.math.BigDecimal;
-import java.util.Collections;
-import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -36,66 +26,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class DemoController {
 
     @Autowired
-    private DefaultThreadPoolProperties defaultThreadPoolProperties;
-
-    @Autowired
-    private SecretProperties secretProperties;
-
-    @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
     @Autowired
     private CouponProperties couponProperties;
 
     @Autowired
-    private RedissonClient redissonClient;
-
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    @Autowired
     private CouponService couponService;
 
     @Autowired
     private MallPublisher mallPublisher;
-
-    @Autowired
-    private CouponMapper couponMapper;
-
-    @GetMapping("saveMysql")
-    public void saveMysql() {
-        Coupon coupon = new Coupon();
-        coupon.setActivityCode("");
-        coupon.setChannelId(0L);
-        coupon.setMerchantId(0L);
-        coupon.setTitle("");
-        coupon.setCouponType(CouponType.PLATFORM);
-        coupon.setCouponDiscountType(CouponDiscountType.FULL_REDUCTION);
-        coupon.setAchieveAmount(new BigDecimal("0"));
-        coupon.setDeductionAmount(new BigDecimal("0"));
-        coupon.setReceiveStartTime(new Date());
-        coupon.setReceiveEndTime(new Date());
-        coupon.setCouponUseTimeType(CouponUseTimeType.FIXED);
-        coupon.setUseStartTime(new Date());
-        coupon.setUseEndTime(new Date());
-        coupon.setReceiveAfterDayEffect(0);
-        coupon.setReceiveAfterEffectDay(0);
-        coupon.setInitTotalCouponNumber(0);
-        coupon.setInitSuccess(false);
-        coupon.setCouponLimitReceiveTimeType(CouponLimitReceiveTimeType.FOREVER);
-        coupon.setCouponLimitReceiveNumber(0);
-        coupon.setAllowMemberRangeType(AllowMemberRangeType.ALL);
-        coupon.setCouponDistributeType(CouponDistributeType.RECEIVE);
-        coupon.setAllowProductRangeType(AllowProductRangeType.ALL);
-        coupon.setState(false);
-        coupon.setRemark("");
-        coupon.setVersion(0L);
-        coupon.setCreatorId(0L);
-        coupon.setUpdaterId(0L);
-        coupon.setCreateTime(new Date());
-        coupon.setUpdateTime(new Date());
-        couponService.saveBatch(Collections.singletonList(coupon));
-    }
 
     @GetMapping("/testMsg")
     public void testMsg(String msg) {
