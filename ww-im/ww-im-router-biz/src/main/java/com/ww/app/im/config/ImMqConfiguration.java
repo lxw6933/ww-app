e97211c49b0255623264288da00c169fbbf921df
WW <ww6933@sina.com>
2025-01-14 22:13:57 +0800

    调整

--- ww-im/ww-im-router-biz/src/main/java/com/ww/app/im/config/ImMqConfiguration.java
+++ ww-im/ww-im-router-biz/src/main/java/com/ww/app/im/config/ImMqConfiguration.java
@@ -1,12 +1,13 @@
 package com.ww.app.im.config;
 
 import com.ww.app.im.router.api.common.ImRouterMqConstant;
-import com.ww.app.rabbitmq.queue.QueueConstant;
-import com.ww.app.rabbitmq.routekey.RouteKeyConstant;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.DirectRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -31,6 +32,20 @@ public class ImMqConfiguration {
     @Bean
     public Binding imRouterMsgBinding() {
         return BindingBuilder.bind(imRouterMsgQueue()).to(imRouterExchange()).with(ImRouterMqConstant.IM_ROUTER_MSG_KEY);
+    }
+
+    @Bean
+    public DirectRabbitListenerContainerFactory imBatchContainerFactory(ConnectionFactory connectionFactory, MessageConverter converter) {
+        DirectRabbitListenerContainerFactory factory = new DirectRabbitListenerContainerFactory();
+        // 设置连接工厂
+        factory.setConnectionFactory(connectionFactory);
+        // 设置每个队列的消费者数量
+        factory.setConsumersPerQueue(10);
+        // 设置每个消费者的预取消息数量
+        factory.setPrefetchCount(10);
+        // 设置消息转换器
+        factory.setMessageConverter(converter);
+        return factory;
     }
 
 }
--- ww-im/ww-im-router-biz/src/main/java/com/ww/app/im/consumer/ImRouterConsumer.java
+++ ww-im/ww-im-router-biz/src/main/java/com/ww/app/im/consumer/ImRouterConsumer.java
@@ -2,7 +2,6 @@ package com.ww.app.im.consumer;
 
 import com.ww.app.common.constant.Constant;
 import com.ww.app.common.thread.ThreadMdcUtil;
-import com.ww.app.common.utils.json.JacksonUtils;
 import com.ww.app.im.common.ImMsgBody;
 import com.ww.app.im.router.api.common.ImRouterMqConstant;
 import com.ww.app.im.service.ImRouterService;
@@ -13,14 +12,13 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author ww
  * @create 2025-01-14- 18:05
  * @description:
+ *     DirectRabbitListenerContainerFactory：适合高实时性、低延迟的 IM 消息场景（如私聊消息）[不支持批量消费]。
+ *     SimpleRabbitListenerContainerFactory：适合高并发、动态扩展的 IM 消息场景（如群聊消息）。
  */
 @Slf4j
 @Component
@@ -29,18 +27,27 @@ public class ImRouterConsumer {
     @Resource
     private ImRouterService imRouterService;
 
-    @RabbitListener(queues = {ImRouterMqConstant.IM_ROUTER_MSG_QUEUE}, containerFactory = "batchContainerFactory")
-    public void imRouterMsg(List<Message> msgList) throws IOException {
-        List<ImMsgBody> imMsgBodyList = new ArrayList<>();
-        for (Message message : msgList) {
-            MessageProperties properties = message.getMessageProperties();
-            String traceId = properties.getHeader(Constant.TRACE_ID);
-            ThreadMdcUtil.setTraceId(traceId);
-            ImMsgBody msg = JacksonUtils.parseObject(message.getBody(), ImMsgBody.class);
-            log.info("消费消息[{}]", msg);
-            imMsgBodyList.add(msg);
-        }
-        imRouterService.batchSendMsg(imMsgBodyList);
+//    @RabbitListener(queues = {ImRouterMqConstant.IM_ROUTER_MSG_QUEUE}, containerFactory = "imBatchContainerFactory")
+//    public void imRouterMsg(List<Message> msgList) throws IOException {
+//        List<ImMsgBody> imMsgBodyList = new ArrayList<>();
+//        for (Message message : msgList) {
+//            MessageProperties properties = message.getMessageProperties();
+//            String traceId = properties.getHeader(Constant.TRACE_ID);
+//            ThreadMdcUtil.setTraceId(traceId);
+//            ImMsgBody msg = JacksonUtils.parseObject(message.getBody(), ImMsgBody.class);
+//            log.info("消费消息[{}]", msg);
+//            imMsgBodyList.add(msg);
+//        }
+//        imRouterService.batchSendMsg(imMsgBodyList);
+//    }
+
+    @RabbitListener(queues = {ImRouterMqConstant.IM_ROUTER_MSG_QUEUE}, containerFactory = "imBatchContainerFactory")
+    public void imRouterMsg(Message message, ImMsgBody imMsgBody) {
+        MessageProperties properties = message.getMessageProperties();
+        String traceId = properties.getHeader(Constant.TRACE_ID);
+        ThreadMdcUtil.setTraceId(traceId);
+        log.info("消费消息[{}]", imMsgBody);
+        imRouterService.sendMsg(imMsgBody);
     }
 
 }
