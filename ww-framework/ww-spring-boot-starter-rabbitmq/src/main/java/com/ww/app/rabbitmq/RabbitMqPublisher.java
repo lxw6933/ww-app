1dbae0776ab8ec20903f8c5d24b5a60271f64c3c
WW <ww6933@sina.com>
2025-01-15 11:14:21 +0800

    调整消息业务处理由rpc远程更改为消息队列

--- ww-framework/ww-spring-boot-starter-rabbitmq/src/main/java/com/ww/app/rabbitmq/RabbitMqPublisher.java
+++ ww-framework/ww-spring-boot-starter-rabbitmq/src/main/java/com/ww/app/rabbitmq/RabbitMqPublisher.java
@@ -11,7 +11,7 @@ import javax.annotation.Resource;
 /**
  * @author ww
  * @create 2023-12-13- 18:18
- * @description:
+ * @description: rabbitmq消息发送者
  */
 @Slf4j
 @Component
@@ -23,14 +23,42 @@ public class RabbitMqPublisher {
     @Resource
     private MessagePostProcessor correlationIdProcessor;
 
+    /**
+     * 使用默认交换机发送到队列【没有声明绑定关系的队列】
+     *
+     * @param queueName 队列名称
+     * @param msg 消息
+     * @param <T> 消息类型
+     */
+    public <T> void sendMsg(String queueName, T msg) {
+        // 自定义消息id
+        MyCorrelationData<T> msgData = new MyCorrelationData<>(true);
+        msgData.setExchange(rabbitTemplate.getExchange());
+        msgData.setMessage(msg);
+        msgData.setRoutingKey(queueName);
+        rabbitTemplate.convertAndSend(queueName, msg, correlationIdProcessor, msgData);
+    }
+
+    /**
+     * 使用指定交换机发送到队列【声明了绑定关系的队列】
+     *
+     * @param exchange 交换机【TopicExchange】
+     * @param routeKey 路由key
+     * @param msg 消息
+     * @param <T> 消息类型
+     */
     public <T> void sendMsg(String exchange, String routeKey, T msg) {
         sendDelayMsg(exchange, routeKey, msg, 0);
     }
 
     /**
-     * 延迟消息发送
+     * 使用自定义交换机发送到队列【声明了绑定关系的队列】
      *
+     * @param exchange 自定义交换机【CustomExchange】
+     * @param routeKey 路由key
+     * @param msg 消息
      * @param delayTime 延时时长 【单位：秒】
+     * @param <T> 消息类型
      */
     public <T> void sendDelayMsg(String exchange, String routeKey, T msg, int delayTime) {
         // 自定义消息id
