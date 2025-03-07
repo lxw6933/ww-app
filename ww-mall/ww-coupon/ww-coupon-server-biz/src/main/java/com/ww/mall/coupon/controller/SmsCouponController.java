8f05977dc48050874ddcfd4cf7c1495f6a6dacc2
WW <ww6933@sina.com>
2025-03-07 13:38:53 +0800

    添加swagger接口文档注解

--- ww-mall/ww-coupon/ww-coupon-server-biz/src/main/java/com/ww/mall/coupon/controller/SmsCouponController.java
+++ ww-mall/ww-coupon/ww-coupon-server-biz/src/main/java/com/ww/mall/coupon/controller/SmsCouponController.java
@@ -7,6 +7,11 @@ import com.ww.mall.coupon.view.bo.SmsCouponCodeListBO;
 import com.ww.mall.coupon.view.bo.SmsCouponPageBO;
 import com.ww.mall.coupon.view.vo.SmsCouponCodeListVO;
 import com.ww.mall.coupon.view.vo.SmsCouponPageVO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,35 +20,47 @@ import java.util.List;
 /**
  * @author ww
  * @create 2023-07-25- 10:23
- * @description:
+ * @description: 优惠券接口
  */
 @RestController
 @RequestMapping("/coupon")
+@Tag(name = "优惠券服务 API")
 public class SmsCouponController {
 
     @Autowired
     private SmsCouponService smsCouponService;
 
+    @Operation(summary = "平台优惠券列表")
     @GetMapping("/activity")
     public AppPageResult<SmsCouponPageVO> pageList(SmsCouponPageBO smsCouponPageBO) {
         return smsCouponService.pageList(smsCouponPageBO);
     }
 
+    @Operation(summary = "查看平台优惠券活动券码列表")
     @GetMapping("/activity/codes")
     public List<SmsCouponCodeListVO> codeList(SmsCouponCodeListBO smsCouponCodeListBO) {
         return smsCouponService.codeList(smsCouponCodeListBO);
     }
 
+    @Operation(summary = "添加平台优惠券活动")
     @PutMapping("/activity")
     public boolean add(@RequestBody SmsCouponActivityAddBO smsCouponActivityAddBO) {
         return smsCouponService.add(smsCouponActivityAddBO);
     }
 
+    @Operation(summary = "领取优惠券")
+    @Parameters({
+            @Parameter(name = "activityCode", description = "平台优惠券活动编码", required = true, in = ParameterIn.QUERY),
+    })
     @GetMapping("/receiveCoupon")
     public boolean receiveCoupon(@RequestParam("activityCode") String activityCode) {
         return smsCouponService.receiveCoupon(activityCode);
     }
 
+    @Operation(summary = "兑换优惠券")
+    @Parameters({
+            @Parameter(name = "couponCode", description = "平台优惠券券码", required = true, in = ParameterIn.QUERY),
+    })
     @GetMapping("/convertCoupon")
     public boolean convertCoupon(@RequestParam("couponCode") String couponCode) {
         return smsCouponService.convertCoupon(couponCode);
