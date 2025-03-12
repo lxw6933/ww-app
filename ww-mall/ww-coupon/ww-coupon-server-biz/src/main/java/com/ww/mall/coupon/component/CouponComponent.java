240cc82355ded3da2d1541c8df7fada87210adf3
WW <ww6933@sina.com>
2025-03-12 16:46:09 +0800

    优惠券模块

---
+++ ww-mall/ww-coupon/ww-coupon-server-biz/src/main/java/com/ww/mall/coupon/component/CouponComponent.java
@@ -0,0 +1,123 @@
+package com.ww.mall.coupon.component;
+
+import com.mongodb.client.result.UpdateResult;
+import com.ww.app.common.enums.GlobalResCodeConstants;
+import com.ww.app.common.exception.ApiException;
+import com.ww.app.mongodb.common.BaseDoc;
+import com.ww.mall.coupon.component.key.CouponRedisKeyBuilder;
+import com.ww.mall.coupon.entity.SmsCouponRecord;
+import com.ww.mall.coupon.eunms.CouponStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author ww
+ * @create 2025-03-12- 15:49
+ * @description: 优惠券组件
+ */
+@Slf4j
+@Component
+public class CouponComponent {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    private CouponRedisKeyBuilder couponRedisKeyBuilder;
+
+    @Resource
+    private MongoTemplate mongoTemplate;
+
+    /**
+     * 冻结优惠券
+     *
+     * @param userId 用户id
+     * @param couponRecordId 用户优惠券id
+     * @param submit 是否提交订单
+     * @return boolean
+     */
+    public SmsCouponRecord freezeMemberCoupon(Long userId, String couponRecordId, boolean submit) {
+        SmsCouponRecord memberCouponRecord = mongoTemplate.findOne(BaseDoc.buildIdQuery(couponRecordId), SmsCouponRecord.class);
+        if (memberCouponRecord == null) {
+            throw new ApiException("优惠券不存在");
+        }
+        if (!userId.equals(memberCouponRecord.getMemberId())) {
+            throw new ApiException(GlobalResCodeConstants.ILLEGAL_REQUEST);
+        }
+        if (CouponStatus.USED.equals(memberCouponRecord.getCouponStatus())) {
+            throw new ApiException("优惠券已使用");
+        }
+        Date now = new Date();
+        if (memberCouponRecord.getUseStartTime().after(now)) {
+            throw new ApiException("优惠券还没到使用时间");
+        }
+        if (memberCouponRecord.getUseEndTime().before(now)) {
+            throw new ApiException("优惠券已过期");
+        }
+        if (submit) {
+            String freezeKey = couponRedisKeyBuilder.buildCouponFreezeKey(couponRecordId);
+            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(freezeKey, Boolean.TRUE.toString(), 1, TimeUnit.DAYS);
+            if (Boolean.TRUE.equals(result)) {
+                log.info("冻结用户[{}]优惠券[{}]成功", userId, couponRecordId);
+            } else {
+                log.error("用户[{}]优惠券[{}]已经被占用", userId, couponRecordId);
+                throw new ApiException("优惠券已被占用");
+            }
+        }
+        return memberCouponRecord;
+    }
+
+    /**
+     * 解冻优惠券
+     *
+     * @param couponRecordId 用户优惠券id
+     */
+    public void unFreezeMemberCoupon(String couponRecordId) {
+        String freezeKey = couponRedisKeyBuilder.buildCouponFreezeKey(couponRecordId);
+        if (Boolean.FALSE.equals(stringRedisTemplate.hasKey(freezeKey))) {
+            log.info("优惠券[{}]无需解冻", couponRecordId);
+            return;
+        }
+        Boolean result = stringRedisTemplate.delete(freezeKey);
+        if (Boolean.TRUE.equals(result)) {
+            log.info("解冻优惠券[{}]成功", couponRecordId);
+        } else {
+            log.error("解冻优惠券[{}]失败", couponRecordId);
+        }
+    }
+
+    /**
+     * 支付更新优惠券状态
+     *
+     * @param couponRecordId 优惠券id
+     */
+    public void updateMemberCouponUseStatus(String couponRecordId) {
+        UpdateResult updateResult = mongoTemplate.updateFirst(BaseDoc.buildIdQuery(couponRecordId), SmsCouponRecord.buildStatusUpdate(CouponStatus.USED), SmsCouponRecord.class);
+        if (updateResult.getModifiedCount() == 1) {
+            log.info("优惠券[{}]状态更新已使用状态成功", couponRecordId);
+        } else {
+            log.error("优惠券[{}]状态更新已使用状态失败", couponRecordId);
+        }
+    }
+
+    /**
+     * 售后回滚优惠券状态
+     *
+     * @param couponRecordId 优惠券id
+     */
+    public void updateMemberCouponRollbackStatus(String couponRecordId) {
+        UpdateResult updateResult = mongoTemplate.updateFirst(BaseDoc.buildIdQuery(couponRecordId), SmsCouponRecord.buildStatusUpdate(CouponStatus.IN_EFFECT), SmsCouponRecord.class);
+        if (updateResult.getModifiedCount() == 1) {
+            log.info("回滚优惠券[{}]状态成功", couponRecordId);
+        } else {
+            log.error("回滚优惠券[{}]状态失败", couponRecordId);
+        }
+    }
+
+}
--- ww-mall/ww-coupon/ww-coupon-server-biz/src/main/java/com/ww/mall/coupon/component/key/CouponRedisKeyBuilder.java
+++ ww-mall/ww-coupon/ww-coupon-server-biz/src/main/java/com/ww/mall/coupon/component/key/CouponRedisKeyBuilder.java
@@ -14,6 +14,7 @@ public class CouponRedisKeyBuilder extends RedisKeyBuilder {
 
     private static final String COUPON_CODE_KEY = "coupon_code";
     private static final String COUPON_NUMBER_KEY = "coupon_number";
+    private static final String COUPON_FREEZE_KEY = "coupon_freeze";
 
     /**
      * 获取券码对应的RSet key
@@ -26,8 +27,24 @@ public class CouponRedisKeyBuilder extends RedisKeyBuilder {
         return super.getPrefix() + StrUtil.join(SPLIT_ITEM, COUPON_CODE_KEY, activityCode, batchNo);
     }
 
+    /**
+     * 获取优惠券活动库存数量key
+     *
+     * @param activityCode 活动编码
+     * @return key
+     */
     public String buildCouponNumberKey(String activityCode) {
         return super.getPrefix() + StrUtil.join(SPLIT_ITEM, COUPON_NUMBER_KEY, activityCode);
+    }
+
+    /**
+     * 获取冻结用户优惠券key
+     *
+     * @param couponRecordId 用户优惠券id
+     * @return key
+     */
+    public String buildCouponFreezeKey(String couponRecordId) {
+        return super.getPrefix() + StrUtil.join(SPLIT_ITEM, COUPON_FREEZE_KEY, couponRecordId);
     }
 
 }
