b623acbb5fa97326dd81fe52c7d9bddec2c8c451
WW <ww6933@sina.com>
2025-09-03 13:49:24 +0800

    refactor: 更新 SmsCouponApi 的 fallbackFactory 实现类路径

--- ww-mall/ww-coupon/ww-coupon-server-api/src/main/java/com/ww/mall/coupon/rpc/SmsCouponApi.java
+++ ww-mall/ww-coupon/ww-coupon-server-api/src/main/java/com/ww/mall/coupon/rpc/SmsCouponApi.java
@@ -1,7 +1,7 @@
 package com.ww.mall.coupon.rpc;
 
 import com.ww.app.common.common.Result;
-import com.ww.app.third.sms.fallback.SmsApiFallBack;
+import com.ww.mall.coupon.fallback.SmsCouponApiFallBack;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @create 2024-11-15- 14:39
  * @description:
  */
-@FeignClient(value = "ww-coupon-server", fallbackFactory = SmsApiFallBack.class)
+@FeignClient(value = "ww-coupon-server", fallbackFactory = SmsCouponApiFallBack.class)
 public interface SmsCouponApi {
 
     @GetMapping("/ww-coupon-server/smsCoupon/receiveCoupon")
