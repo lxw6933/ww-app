92c8d8069a8a8391773a86682c243f63702051d2
ww <ww6933@sina.com>
2025-09-17 14:43:32 +0800

    refactor: 更新 ExcelMinioTemplate 注册逻辑，替换 ConditionalOnClass 为 ConditionalOnBean

--- ww-framework/ww-spring-boot-starter-excel/src/main/java/com/ww/app/excel/EasyExcelAutoConfiguration.java
+++ ww-framework/ww-spring-boot-starter-excel/src/main/java/com/ww/app/excel/EasyExcelAutoConfiguration.java
@@ -1,9 +1,9 @@
 package com.ww.app.excel;
 
 import com.ww.app.excel.aspect.ExcelHandlerAspect;
-import io.minio.MinioClient;
+import com.ww.app.minio.MinioTemplate;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,7 +23,7 @@ public class EasyExcelAutoConfiguration {
     }
 
     @Bean
-    @ConditionalOnClass(MinioClient.class)
+    @ConditionalOnBean(MinioTemplate.class)
     public ExcelMinioTemplate excelMinioTemplate() {
         log.info("初始化excelMinioTemplate功能成功...");
         return new ExcelMinioTemplate();
