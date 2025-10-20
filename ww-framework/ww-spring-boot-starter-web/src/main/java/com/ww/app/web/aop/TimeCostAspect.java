30a29e38c1ccc8670c55a8212585ce98b5a1888f
ww <ww6933@sina.com>
2025-10-20 15:07:02 +0800

    feat: 调整TimeCostAspect

--- ww-framework/ww-spring-boot-starter-web/src/main/java/com/ww/app/web/aop/TimeCostAspect.java
+++ ww-framework/ww-spring-boot-starter-web/src/main/java/com/ww/app/web/aop/TimeCostAspect.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author ww
@@ -18,18 +20,39 @@ import java.time.Duration;
 @Component
 public class TimeCostAspect {
 
-    @Around("@annotation(com.ww.app.web.annotation.TimeCost)")
-    public Object exportExcel(ProceedingJoinPoint joinPoint) {
-        long startTime = System.nanoTime();
-        log.info("开始：{}", joinPoint.getSignature().getName());
+    /**
+     * 同时支持方法级与类级的 TimeCost 注解
+     */
+    @Around("@annotation(com.ww.app.web.annotation.TimeCost) || @within(com.ww.app.web.annotation.TimeCost)")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTimeNs = System.nanoTime();
+
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Class<?> targetClass = methodSignature.getDeclaringType();
+        String methodName = methodSignature.getName();
+        String className = targetClass.getSimpleName();
+
+        log.info("[TimeCost] 开始执行 {}#{}", className, methodName);
+
         try {
-            return joinPoint.proceed();
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
-        } finally {
-            long cost = System.nanoTime() - startTime;
-            Duration time = Duration.ofNanos(cost);
-            log.info("结束，消耗了：{}s  {}ms", time.getSeconds(), cost);
+            Object result = joinPoint.proceed();
+            long costNs = System.nanoTime() - startTimeNs;
+            long costMs = TimeUnit.NANOSECONDS.toMillis(costNs);
+            Duration duration = Duration.ofNanos(costNs);
+
+            // 超过阈值则以 warn 打印（默认 1s 阈值）
+            long warnThresholdMs = 1000L;
+            if (costMs >= warnThresholdMs) {
+                log.warn("[TimeCost] 结束 {}#{} 耗时: {} ms (~{} s)", className, methodName, costMs, duration.getSeconds());
+            } else {
+                log.info("[TimeCost] 结束 {}#{} 耗时: {} ms", className, methodName, costMs);
+            }
+            return result;
+        } catch (Throwable ex) {
+            long costNs = System.nanoTime() - startTimeNs;
+            long costMs = TimeUnit.NANOSECONDS.toMillis(costNs);
+            log.error("[TimeCost] 异常 {}#{} 耗时: {} ms, 异常: {}", className, methodName, costMs, ex.toString());
+            throw ex;
         }
     }
 
